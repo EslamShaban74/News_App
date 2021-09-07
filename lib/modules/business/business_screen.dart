78e3a8fb649a13d649cd3471ef55a9c66aa72ab7
Eslam <eslamshaban12328@gmail.com>
2021-09-07 19:56:14 +0200

    News App using API (Business - Sports- Science)

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -14,6 +14,7 @@ class BusinessScreen extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         var list = NewsCubit.get(context).business;
+
         return buildArticle(list,context);
       },
     );
