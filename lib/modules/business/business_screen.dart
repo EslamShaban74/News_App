d187fd6608742852a367223c4c775be14e10a57f
Eslam <eslamshaban12328@gmail.com>
2021-09-07 19:57:29 +0200

    News App using API (Business - Sports- Science)

--- lib/modules/business/business_screen.dart
+++ lib/modules/business/business_screen.dart
@@ -1,11 +1,9 @@
-import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/cubit/cubit.dart';
 import 'package:news_app/cubit/states.dart';
 import 'package:news_app/shared/components/components.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 class BusinessScreen extends StatelessWidget {
   @override
